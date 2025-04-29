680d07180c619fdd02a3aad97b46c8831b5b1455
Haotian-Ma <dlwlrmaqa@gmail.com>
2025-04-29 21:03:12 +1000

    default API_BASE_URL to empty string and remove production VITE_API_BASE_URL

--- frontend/src/views/VegetationMap.vue
+++ frontend/src/views/VegetationMap.vue
@@ -14,7 +14,7 @@
   </template>
   
   <script setup>
-  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? ''
   const mapUrl = `${API_BASE_URL}/api/map`
   </script>
   
